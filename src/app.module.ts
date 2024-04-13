00e05b2a5a910739c40e35d0446a5c9693fc7562
okcoder <okcoder@live.com>
2024-04-13 10:33:17 +0900

    add sse client
    visit http://localhost:3000/sse.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [HelloModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    HelloModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
